d43731f24cfdf36cef2172ccfb465dae3cf42a38
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2024-02-08 13:14:59 -0800

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('client')); // Serve the client files
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Define a route handler for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.use('/socket.io' ,express.static('client')); // Serve the client files
 // console.log(app);
 console.log(http);
 io.on('connection', (socket) => {
