249d321b3bb1946686dd70accd56f1501fce112c
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2024-02-08 11:15:36 -0800

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,3 +1,20 @@
-export function onRequest(context) {
-  return new Response("Hello, world!")
-}
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('client')); // Serve the client files
+// console.log(app);
+console.log(http);
+io.on('connection', (socket) => {
+
+  console.log("connecting");
+
+  socket.on('draw', (data) => {
+
+    socket.broadcast.emit('draw', data); // Broadcast to all other clients
+
+  });
+});
+
+http.listen(3000, () => console.log('Server listening on port 3000'));
